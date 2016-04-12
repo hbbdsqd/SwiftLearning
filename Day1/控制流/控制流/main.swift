09774d8436479a137142ca78924476d3558c3b91
小东门儿 <hbbdsqd@163.com>
2016-04-12 15:47:26 +0800

    简单值

---
+++ Day1/控制流/控制流/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  控制流
+//
+//  Created by 苏秋东 on 16/4/12.
+//  Copyright © 2016年 com.hyde.carelink. All rights reserved.
+//
+
+import Foundation
+
+
+
---
+++ Day1/简单值/Project1/main.swift
@@ -0,0 +1,118 @@
+//
+//  main.swift
+//  Project1
+//
+//  Created by 苏秋东 on 16/4/12.
+//  Copyright © 2016年 com.hyde.carelink. All rights reserved.
+//
+
+import Foundation
+
+/*
+//var 定义变量
+var obj = 1;
+//let 定义常量
+let newObj : Float  = 3.0;
+//print(obj);
+//print(newObj);
+*/
+
+/*
+let lable = "the width is ";
+let width = 94;
+let widthLable = lable + String(width);
+//print(widthLable);
+*/
+
+/*
+//使用\()类型转换
+let one = 1.0;
+let friend = "friends";
+let lable = "I have \(one + 2) "+friend;
+print(lable);
+*/
+
+/*
+//创建数组
+var objList = ["one","two","three"];
+objList[1] = "newTwo";
+print(objList[1]);
+var emptyArray = [String]();
+emptyArray.append("summer");
+emptyArray.append("11");
+print(emptyArray);
+emptyArray.removeAtIndex(1);
+print(emptyArray);
+*/
+
+
+/*
+//创建字典
+var dic = ["name":"summer","age":12];
+dic["name"] = "hbbdsqd";
+//print(dic["name"]);
+var emptyDic = [String:Float]();
+emptyDic["name"] = 123;
+print(emptyDic);
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
